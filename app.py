2c1c8a86d28de1e25eeec769acc6c17e76ee1efe
Mark Black <32424631+markblack96@users.noreply.github.com>
2018-11-21 23:03:54 -0600

    Delete app.py

--- app.py
+++
@@ -1,9 +0,0 @@
-import os
-from flask import Flask, render_template, request
-
-app = Flask(__name__)
-app.config['SECRET_KEY'] = 'secret' # this needs to be in separate file soon
-
-@app.route('/'):
-def index():
-    return "Hello"
